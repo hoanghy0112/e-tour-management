e4fb65931a1373ec8f7d8e1c73875a0c16378f38
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2023-06-20 06:30:28 +0700

    Add ban page

---
+++ src/components/BanPage/BanPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './BanPage.module.scss';
+
+const BanPage = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Your company is being ban.</h1>
+            <p>Please contact our support center to get help.</p>
+            <p>
+                Email to{' '}
+                <a
+                    style={{
+                        textDecoration: 'underline',
+                    }}
+                    href="mailto:21522266@gm.uit.edu.vn"
+                >
+                    this!
+                </a>
+            </p>
+        </div>
+    );
+};
+
+export default BanPage;
