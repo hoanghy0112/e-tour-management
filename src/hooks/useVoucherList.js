32c4199d2686adb320e3413a02f83f388e628409
Tuyet Mai <tranthituyetmai7430@gmail.com>
2023-06-30 20:07:54 +0700

    fix: return voucher list of staff company

--- src/hooks/useVoucherList.js
+++ src/hooks/useVoucherList.js
@@ -1,22 +1,22 @@
-import { useState } from "react";
-import useSocket from "./useSocket";
+import { useState } from 'react';
+import useSocket from './useSocket';
 
 export default function useVoucherList({ num = 1000 }) {
-	const [vouchers, setVouchers] = useState([]);
-	const [error, setError] = useState(null);
+    const [vouchers, setVouchers] = useState([]);
+    const [error, setError] = useState(null);
 
-	useSocket(
-		(socket) => {
-			socket.emit("view-new-voucher", { num });
-			socket.on("new-voucher-list", (data) => {
-				setVouchers(data.data);
-			});
-			socket.on("error", (error) => {
-				setError(error);
-			});
-		},
-		[vouchers.join(""), num]
-	);
+    useSocket(
+        (socket) => {
+            socket.emit('view-company-voucher', { num });
+            socket.on('view-company-voucher-result', (data) => {
+                setVouchers(data.data);
+            });
+            socket.on('error', (error) => {
+                setError(error);
+            });
+        },
+        [vouchers.join(''), num]
+    );
 
-	return { data: vouchers, isError: error !== null, error };
+    return { data: vouchers, isError: error !== null, error };
 }
