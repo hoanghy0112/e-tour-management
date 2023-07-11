e418d36dc767a4a5d9b9767b28393fa8d163f270
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2023-07-11 11:07:58 +0700

    Turn on strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import RouteList from "./components/RouteList/RouteList";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
